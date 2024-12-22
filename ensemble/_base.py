b4804b0db53d359ae9124fb2f27a87409f955da4
samanemami <saman.emami@hotmail.com>
2024-12-22 11:24:45 +0100

    Changes to be committed: 	modified:   ensemble/_base.py

--- ensemble/_base.py
+++ ensemble/_base.py
@@ -94,6 +94,7 @@ class BaseMTGB(BaseGradientBoosting):
         self.is_classifier = False
         self.early_stopping = early_stopping
 
+        self.eta = 0.0001
         np.random.seed(self.random_state)
 
     @abstractmethod
@@ -199,7 +200,7 @@ class BaseMTGB(BaseGradientBoosting):
                 raw_predictions,
                 sample_weight,
                 sample_mask,
-                learning_rate=self.learning_rate,
+                learning_rate=self.learning_rate if task_type == "data_pooling" else self.eta,
                 k=k,
             )
 
@@ -784,7 +785,8 @@ class BaseMTGB(BaseGradientBoosting):
                         idx_r = t == r_label
 
                         X_r = self.X_test[idx_r]
-                        rh[idx_r] += self.learning_rate * tree.predict(X_r)
+                        # rh[idx_r] += self.learning_rate * tree.predict(X_r)
+                        rh[idx_r] += self.eta * tree.predict(X_r)
 
                         ch[idx_r] = ensemble_pred(
                             (self.sigmas_[i, r]),
